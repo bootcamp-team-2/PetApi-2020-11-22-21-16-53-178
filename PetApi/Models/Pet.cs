67de1e86d73d615b5c0848f9f0076f39e48f211e
Lin Liu <LLiu41@slb.com>
2020-11-23 17:35:41 +0800

    Feat: implement get all pets API

---
+++ PetApi/Models/Pet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetApi.Models
+{
+    public class Pet
+    {
+        public Pet()
+        {
+        }
+
+        public Pet(string name, string type, string color, int price)
+        {
+            Name = name;
+            Type = type;
+            Color = color;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+        public int Price { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals((Pet)obj);
+        }
+
+        protected bool Equals(Pet other)
+        {
+            return this.Name == other.Name
+                   && this.Type == other.Type
+                   && this.Color == other.Color
+                   && this.Price == other.Price;
+        }
+    }
+}
